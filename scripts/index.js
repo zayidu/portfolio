dad45d779ed43b264d11185abfef588c77bfc4e5
Zayidu Ansari <zayidu11@gmail.com>
2020-06-27 00:08:06 +0400

    Added index.js to open Images and Added The nav for Mobiles and commented

---
+++ scripts/index.js
@@ -0,0 +1,15 @@
+const onOpenInsta = () => {
+  window.open('https://instaclonezayidu.herokuapp.com/', '_blank').focus();
+};
+
+const onOpenzDevConnector = () => {
+  window.open('https://powerful-brook-38361.herokuapp.com/', '_blank').focus();
+};
+
+const onOpenNBA = () => {
+  window.open('https://nba-full-stack-59c4b.web.app/', '_blank').focus();
+};
+
+const onOpenBookaholic = () => {
+  window.open('https://fierce-brook-03104.herokuapp.com/', '_blank').focus();
+};
